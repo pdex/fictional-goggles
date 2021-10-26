cda44a0428b3923c698fcf90f2733ce489374373
Chris Keating <christopherkeating@gmail.com>
2021-10-26 18:01:39 -0500

    Update rcs-tf-setup.sh

--- rcs-tf-setup.sh
+++ rcs-tf-setup.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/bash -eux
 
 PROJECT_ID=$1
+
+echo "enabling gcloud"
+gcloud config set project $PROJECT_ID
+gcloud auth login
+
 
 echo "enabling services"
 gcloud services enable spanner.googleapis.com
@@ -16,13 +21,9 @@ gcloud services enable cloudbuild.googleapis.com
 
 echo "enabling iam"
 gcloud iam service-accounts create terraform-admin
-
 gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:terraform-admin@$PROJECT_ID.iam.gserviceaccount.com --role=roles/editor
-
 gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:terraform-admin@$PROJECT_ID.iam.gserviceaccount.com --role=roles/iam.securityAdmin
 
 echo "creating terraform instance"
 
 gcloud beta compute --project=$PROJECT_ID instances create tf-runner --zone=us-central1-a --machine-type=n1-standard-2 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=terraform-admin@$PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=tf-runner --reservation-affinity=any
-
-echo "gcloud compute ssh tf-runner --zone=us-central1-a"
